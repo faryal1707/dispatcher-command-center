58ce2350fca4e542e98a59bd59f9e5516c7e0941
Faryal Adnan <190634870+faryal1707@users.noreply.github.com>
2026-09-09 19:05:55 -0400

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+window.DISPATCHOS_SUPABASE_URL =
+  'https://evcshsbyqkzhjtibfugo.supabase.co/rest/v1/';
+
+window.DISPATCHOS_SUPABASE_ANON_KEY =
+  'sb_publishable_tBxO4H102Drs2cZqm5ZoJw_Uj69EE4C';
